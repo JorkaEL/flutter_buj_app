e92a8dfe40fae9f8d6ec5ec799eb657094241407
Jorka <xboutanquoy@gmail.com>
2019-12-08 17:24:15 +0100

    create model

---
+++ lib/model/key.dart
@@ -0,0 +1,7 @@
+class Key {
+  num id;
+  String libelle;
+  String icon;
+
+  Key({this.id, this.libelle, this.icon});
+}
---
+++ lib/model/tracker.dart
@@ -0,0 +1,7 @@
+class Tracker {
+  num id;
+  String libelle;
+  String color;
+
+  Tracker({this.id, this.libelle, this.color});
+}

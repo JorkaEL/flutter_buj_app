5c9b82d2be19be1a2c2d6e2289b03430c461da13
Jorka <xboutanquoy@gmail.com>
2020-02-18 19:49:17 +0100

    refacto(): indent

--- lib/ui/task/component/task_add/task_add_dropdown.dart
+++ lib/ui/task/component/task_add/task_add_dropdown.dart
@@ -5,13 +5,8 @@ import 'package:flutter_buj_app/util/routing_constants.dart';
 typedef TaskAddCallBack(add);
 
 class TaskAddDropdown extends StatefulWidget {
-  TaskAddDropdown(
-      list,
-      choice,
-      libelle,
-      route,
-      callback) :
-        this.list = list,
+  TaskAddDropdown(list, choice, libelle, route, callback)
+      : this.list = list,
         this.choice = choice,
         this.libelle = libelle,
         this.route = route,
@@ -23,40 +18,40 @@ class TaskAddDropdown extends StatefulWidget {
   String route;
   TaskAddCallBack callback;
 
-
   @override
   _TaskAddDropdown createState() => _TaskAddDropdown();
 }
 
-
 class _TaskAddDropdown extends State<TaskAddDropdown> {
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          DropdownButton (
-             hint: Text(widget.libelle),
-            value: widget.choice,
-            onChanged: (val) {
-              widget.callback(val);
-            },
-            items: widget.list.map((el) {
-              return new DropdownMenuItem(
-                  value: el,
-                  child: Text(el.libelle),
-              );
-            }).toList(),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        DropdownButton(
+          hint: Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Text(widget.libelle),
           ),
-          IconButton(
-            icon: Icon(Icons.add_circle),
-            color: Colors.deepPurple,
-            onPressed: () {
-              Navigator.pushNamed(context, widget.route, arguments: TaskAddPageRoute);
-            },
-          ),
-        ],
-      ),
+          value: widget.choice,
+          onChanged: (val) {
+            widget.callback(val);
+          },
+          items: widget.list.map((el) {
+            return new DropdownMenuItem(
+              value: el,
+              child: Text(el.libelle),
+            );
+          }).toList(),
+        ),
+        IconButton(
+          icon: Icon(Icons.add_circle),
+          color: Colors.deepPurple,
+          onPressed: () {
+            Navigator.pushNamed(context, widget.route,
+                arguments: TaskAddPageRoute);
+          },
+        ),
+      ],
     );
   }
 }
--- lib/ui/task/task_add_page.dart
+++ lib/ui/task/task_add_page.dart
@@ -11,22 +11,17 @@ import 'package:intl/intl.dart';
 import 'package:logger/logger.dart';
 
 class TaskAddPage extends StatefulWidget {
-
   @override
   _TaskAddState createState() => _TaskAddState();
 }
 
-
 class _TaskAddState extends State<TaskAddPage> {
-
-
   final logger = Logger();
   final _formKey = GlobalKey<FormState>();
   final format = DateFormat("yyyy-MM-dd");
 
   static DateTime now = DateTime.now();
   DateTime _selectedDate = DateTime(now.year, now.month, now.day);
-
 
   final libelle = TextEditingController();
 
@@ -61,66 +56,78 @@ class _TaskAddState extends State<TaskAddPage> {
     this.keyList = BujService().getKeyTask();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.pushNamed(context, TaskPageRoute)
-          ),
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.pushNamed(context, TaskPageRoute)),
           backgroundColor: Colors.deepPurple,
           centerTitle: true,
-          title: Text('Ajout de tache')
-      ),
-        body: Form(
-        key: _formKey,
-        child: Container(
-          padding: EdgeInsetsDirectional.only(start: 15.0, top: 5.0, end: 15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                  controller: this.libelle,
-                  decoration: const InputDecoration(
-                    hintText: 'Libéller de la tache',
-                  ),
-                // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'SVP rentrer un libéller pour la tache';
-                    }
-                    return null;
-                  },
-                ),
-              ButtonSelectDate(callback: (day) => changeDay(day), selectedDate: _selectedDate),
-              TaskAddDropdown(keyList, keyChoice, 'Sélectionner votre clée', KeyTaskAddPageRoute, (keyTask) => changeKeyTask(keyTask)),
-              TaskAddDropdown(habitList, habitChoice, 'Sélectionner votre habitude', HabitAddPageRoute, (hab) => changeHabit(hab)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          title: Text('Ajout de tache')),
+      body: Form(
+          key: _formKey,
+          child: Container(
+              padding:
+                  EdgeInsetsDirectional.only(start: 15.0, top: 5.0, end: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  RaisedButton(
-                    color: Colors.deepPurple,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      // Validate will return true if the form is valid, or false if
-                      // the form is invalid.
-                      if (_formKey.currentState.validate() && this.keyChoice != null && this.habitChoice != null) {
-                        // Process data
-                        var t = Task(libelle: this.libelle.text, date: this._selectedDate, id: 0, state: false, habit: this.habitChoice, key: this.keyChoice );
-                        BujService().addTask(t);
-                        Navigator.pushNamed(context, TaskPageRoute);
+                  TextFormField(
+                    controller: this.libelle,
+                    decoration: const InputDecoration(
+                      hintText: 'Libéller de la tache',
+                    ),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'SVP rentrer un libéller pour la tache';
                       }
+                      return null;
                     },
-                    child: Text('Valider'),
-                ),]
-              ),
-            ],
-          )
-        )// Build this out in the next steps.
-      ),
+                  ),
+                  ButtonSelectDate(
+                      callback: (day) => changeDay(day),
+                      selectedDate: _selectedDate),
+                  TaskAddDropdown(keyList, keyChoice, 'Sélectionner votre clée',
+                      KeyTaskAddPageRoute, (keyTask) => changeKeyTask(keyTask)),
+                  TaskAddDropdown(
+                      habitList,
+                      habitChoice,
+                      'Sélectionner votre habitude',
+                      HabitAddPageRoute,
+                      (hab) => changeHabit(hab)),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        RaisedButton(
+                          color: Colors.deepPurple,
+                          textColor: Colors.white,
+                          onPressed: () {
+                            // Validate will return true if the form is valid, or false if
+                            // the form is invalid.
+                            if (_formKey.currentState.validate() &&
+                                this.keyChoice != null &&
+                                this.habitChoice != null) {
+                              // Process data
+                              var t = Task(
+                                  libelle: this.libelle.text,
+                                  date: this._selectedDate,
+                                  id: 0,
+                                  state: false,
+                                  habit: this.habitChoice,
+                                  key: this.keyChoice);
+                              BujService().addTask(t);
+                              Navigator.pushNamed(context, TaskPageRoute);
+                            }
+                          },
+                          child: Text('Valider'),
+                        ),
+                      ]),
+                ],
+              )) // Build this out in the next steps.
+          ),
     );
   }
-
 }

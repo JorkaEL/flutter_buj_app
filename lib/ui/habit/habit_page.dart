fffe9836ca15294400ff888cd41d1826c293e2f9
Jorka <xboutanquoy@gmail.com>
2020-04-22 14:47:18 +0200

    feat(): add bloc to habit list page and add page

--- lib/ui/habit/habit_page.dart
+++ lib/ui/habit/habit_page.dart
@@ -2,52 +2,66 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_buj_app/model/habit.dart';
 import 'package:flutter_buj_app/ui/component/side_menu.component.dart';
-import 'package:flutter_buj_app/util/buj_service.dart';
+import 'package:flutter_buj_app/ui/habit/bloc/habit_bloc.dart';
+import 'package:flutter_buj_app/ui/habit/component/habit_list.component.dart';
 import 'package:flutter_buj_app/util/routing_constants.dart';
 import 'package:i18n_localizations/i18n_localizations.dart';
 
 class HabitPage extends StatefulWidget {
-
   @override
   _HabitPageState createState() => _HabitPageState();
 }
 
-
 class _HabitPageState extends State {
-
-  List<Habit> habitList;
+  final _bloc = HabitBloc();
 
   @override
   void initState() {
     super.initState();
-    this.habitList = BujService().getHabits();
+    _bloc.updateList();
   }
 
-  @override build(BuildContext context) {
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Colors.deepPurple,
             centerTitle: true,
-            title: Text(I18nLocalizations.translate(context,"habit.title"))
-        ),
+            title: Text(I18nLocalizations.translate(context, "habit.title"))),
         drawer: SideMenu(),
-        body: ListView.separated(
-          scrollDirection: Axis.vertical,
-          separatorBuilder: (context, index) => Divider(),
-          itemCount: habitList.length,
-          itemBuilder: (context, index) => ListTile(
-            title:  Text(habitList[index].libelle),
-            trailing: Icon(Icons.brightness_1, color: habitList[index].color),
-          ),
-        ),
+        body: StreamBuilder<List<Habit>>(
+            stream: _bloc.habitListStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return HabitList(
+                  listHabit: snapshot.data
+                );
+              } else {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(I18nLocalizations.translate(
+                        context, "error.habit.list")),
+                  );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              }
+            }),
         floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.deepPurple,
-      onPressed: () {
-        Navigator.pushNamed(context, HabitAddPageRoute, arguments: HabitPageRoute);
-      },
-      child: Icon(Icons.add),
-    )
-    );
+          backgroundColor: Colors.deepPurple,
+          onPressed: () {
+            Navigator.pushNamed(context, HabitAddPageRoute,
+                arguments: HabitPageRoute);
+          },
+          child: Icon(Icons.add),
+        ));
   }
-
 }
